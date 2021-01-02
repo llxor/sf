68cbab8e4706f0b8f91002ffd32fc0e3e940a33a
llxor <llxoyd@gmail.com>
2021-01-02 17:35:53 +0000

    Move buildcommand to after stradd

--- src/utils.h
+++ src/utils.h
@@ -3,15 +3,6 @@
 
 const char *RED = "\e[31;1m", *BOLD = "\e[37;1m", *NORMAL = "\e[0m";
 
-void buildcommand(char *cmd, int argc, char **argv) {
-  for (int i = 1; i < argc; i++) {
-    stradd(cmd, argv[i], ' ');
-  }
-
-  // capture stdout and stderr
-  stradd(cmd, "2>&1", '\0');
-}
-
 void error(const char *msg) {
   printf("%serror:%s %s\n%s", RED, BOLD, msg, NORMAL);
   exit(1);
@@ -19,4 +10,13 @@ void error(const char *msg) {
 
 void stradd(char *dst, const char *src, const char sep) {
   sprintf(dst, "%s%s%c", dst, src, sep);
+}
+
+void buildcommand(char *cmd, int argc, char **argv) {
+  for (int i = 1; i < argc; i++) {
+    stradd(cmd, argv[i], ' ');
+  }
+
+  // capture stdout and stderr
+  stradd(cmd, "2>&1", '\0');
 }
