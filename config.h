2d902f314b5a32cba7555df316541d55793b29d5
ellxor <ellxor@protonmail.ch>
2021-05-08 22:35:14 +0100

    Change default colors

--- config.h
+++ config.h
@@ -2,7 +2,7 @@
 #define MAX_ERR 20
 #define TABSTOP  8
 
-#define FOREGROUND_COLOR 0
-#define BACKGROUND_COLOR 7 
-#define EXIT_CODE_COLOR  7 
+#define FOREGROUND_COLOR 7
+#define BACKGROUND_COLOR 0
+#define EXIT_CODE_COLOR  0
 #define HIGHLIGHT A_BOLD
